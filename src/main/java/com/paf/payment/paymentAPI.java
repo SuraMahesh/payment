c75b9841574f874e3941c89001c3552fa5d7b144
SuraMahesh <surangamunasinghe22568@gmail.com>
2020-05-06 10:42:28 +0530

    payment form update

--- src/main/java/com/paf/payment/paymentAPI.java
+++ src/main/java/com/paf/payment/paymentAPI.java
@@ -1,5 +1,99 @@
 package com.paf.payment;
 
-public class paymentAPI {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class paymentAPI extends HttpServlet {
+	
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		
+		
+		
+		String NIC      =   request.getParameter("nic");        
+		String FNAME	=	request.getParameter("f_name");
+		String LNAME	=	request.getParameter("l_name");
+		String PATMAIL 	=	request.getParameter("pat_mail");
+		String MOBNUM 	= 	request.getParameter("mob_num");
+		String PBDAY	=	request.getParameter("p_bday");
+		String ADDR 	= 	request.getParameter("addr");
+		String PASS		=	request.getParameter("pass");
+		
+		System.out.println("API CALL  :  " +NIC);
+		
+		CustomerDataModel item = new CustomerDataModel();
+		
+		String output = item.createUser(NIC, FNAME, LNAME, PATMAIL, MOBNUM, PBDAY, ADDR, PASS);
+		
+		
+		response.getWriter().write(output);
+	}
+
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		CustomerDataModel item = new CustomerDataModel();
+		Map paras = getParasMap(request);
+		System.out.println(paras + "  :: paras PUT");
+		String output = item.updateUser(paras.get("hidPatSave").toString(), paras.get("nic").toString(),
+				paras.get("f_name").toString(), paras.get("l_name").toString(), paras.get("pat_mail").toString(),
+				paras.get("mob_num").toString(), paras.get("p_bday").toString(), paras.get("addr").toString(), 
+				paras.get("pass").toString());
+		
+		response.getWriter().write(output);
+	}
+
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		Map paras = getParasMap(request);
+		CustomerDataModel item = new CustomerDataModel();
+		
+		String output = item.delete(paras.get("patID").toString());
+		
+		System.out.println(output + "Delete ID");
+		
+		response.getWriter().write(output);
+		
+		
+	}
+	
+	private static Map getParasMap(HttpServletRequest request) {
+        Map<String, String> map = new HashMap<String, String>();
+        try {
+            Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+            String queryString = scanner.hasNext() ?
+                    scanner.useDelimiter("\\A").next() : "";
+            scanner.close();
+            String[] params = queryString.split("&");
+            for (String param : params) {
+                String[] p = param.split("=");
+                map.put(p[0], p[0]);
+            }
+            
+            System.out.println(map + "Map ***");
+        } catch (IOException e) {
+            e.printStackTrace();
+            
+        }
+        return map;
+        
+    }
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,91 @@
-<html>
+<%@ page import="java.util.List"%>
+<%@ page import="com.paf.payment.paymentDataModel" %>
+
+<!DOCTYPE html>
+<head>
+
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="view/bootstrap.min.css">
+<script src="component/jquery-3.5.0.min.js"></script>
+<script src="component/Patient.js"></script>
+</head>
+
 <body>
-    <h2>Jersey RESTful Web Application!</h2>
-    <p><a href="webapi/myresource">Jersey resource</a>
-    <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
-    for more information on Jersey!
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm">
+				<h3>
+					Patient Payment details Adding Form 
+				</h3>
+
+				<form id="pat_Form"  name="pat_Form" method="post" action="index.jsp">
+
+					<div class="form-group col-md-6">
+						<div class="form-group ">
+							<label for="IdentyCardNumber">Patient ID</label> <input type="text"
+								class="form-control" id="nic" name="nic"
+								placeholder="980451465V" required>
+						</div>
+
+						<div class="form-row">
+							<div class="form-group col-md-6">
+								<label for="firstname">Card Number</label> <input type="text"
+									class="form-control" id="f_name" name="f_name" placeholder="">
+							</div>
+							<div class="form-group col-md-6">
+								<label for="lastname">Name of the card</label> <input type="text"
+									class="form-control" id="l_name" name="l_name" placeholder="">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="patientEmail">Valid date</label> <input type="email"
+								class="form-control" id="pat_mail" name="pat_mail"
+								placeholder="">
+						</div>
+						<div class="form-group">
+							<label for="contactNumber">Cvc code</label> <input
+								type="text" class="form-control" id="mob_num" name="mob_num"
+								placeholder="">
+						</div>
+						<div class="form-group">
+							<label for="patbirthday">Amount</label> <input type="date"
+								class="form-control" id="p_bday" name="p_bday" placeholder="">
+
+						<div class="form-row">
+							<div class="form-group col-md-6">
+								<label for="patpassword">Password</label> <input type="password"
+									class="form-control" id="pass" name="pass" placeholder="">
+							</div>
+						</div>
+						<br>
+						<input id="btnSave" name="btnSave" type="button" value="Save" class="btn btn-primary">
+						<input id="hidPatSave" name="hidPatSave" type="hidden" value="">
+
+					</div>
+				</form>
+
+			</div>
+		</div>
+		<div id="alertSuccess" class="alert alert-success"></div>
+		<div id="alertError" class="alert alert-danger"></div>
+
+		<div class="row">
+			<div class="col-sm">
+				<div id="divItemsGrid">
+					<% 
+ 
+ 						paymentDataModel itemObj = new paymentDataModel();
+ 						out.print(itemObj.getCustomers());
+										
+ 					%>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
 </body>
 </html>
